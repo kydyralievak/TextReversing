bb72c37ffae4dd2210966b0757593df8741303c2
Каныкей <kydyralieavk@gmail.com>
2018-06-07 16:51:40 +0600

    commit project

---
+++ TextReversing/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace TextReversing
+{
+    internal class Program
+    {
+        public static string Reverse(string text)
+        {
+            char[] array = text.ToCharArray();
+            Array.Reverse(array);
+
+            return new String(array);
+        }
+
+        static void Main()
+        {
+            while (true)
+            {
+                Console.WriteLine("Введите текст(для выхода введите \"выход\")");
+                var input = Console.ReadLine();
+
+                if (input == "выход")
+                    break;
+
+                Console.WriteLine($"Перевернутая строка:{Reverse(input)}");
+            }
+        }
+    }
+}
